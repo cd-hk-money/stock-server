351d6bad8a7adf8977e249f105c9ae5d98af0090
97Kzone <mmnm3@naver.com>
2022-04-29 18:42:37 +0900

    22.04.29 테스트 서버

---
+++ process_data.py
@@ -0,0 +1,77 @@
+from collections import defaultdict
+from decimal import Decimal
+
+#날짜별로 묶어주기
+def dateisidx(datas):
+    dict = defaultdict(list)    
+    for data in datas:
+        data = list(data)
+        for i in range(1, len(data)):
+            if type(data[i]) == Decimal:
+                data[i] = float(data[i])
+
+        dict[data[0]].append({"type": data[1], "close": data[2], "open": data[3],\
+             "high": data[4], "low": data[5], "volume": data[6], "changes": data[7]})
+    
+    return dict
+
+#기업 : 코드 매칭
+def codelist(datas):
+    dict = defaultdict(list)
+    for data in datas:
+        data = list(data)
+        dict[data[0]] = data[1]
+    
+    return dict
+
+#오늘의 TOP 10
+def findtop(type, datas):
+    dic = {type: []}
+    for data in datas:
+        data = list(data)
+        dic[type].append(data)
+    
+    return dic
+
+#랜덤 추천종목 key, value 매칭
+def matchrecom(datas):
+    dic = dict()
+    for data in datas:
+        data = list(data)
+
+        if data[0] in dic:
+            dic[data[0]].append({"name": data[1], "close": data[2], "changes_ratio": data[3]})
+        else:
+            dic[data[0]] = {"name": data[1], "close": data[2], "changes_ratio": data[3]}
+    return dic
+
+#그래프 위해 날짜 꺼내주기
+def data2grahp(datas):
+    dict = defaultdict(list)
+    for data in datas:
+        data = list(data)
+        dict[data[0]] = data[1]
+    
+    return dict
+
+#재무제표 key, value 맞춰주기
+def state2dict(datas):
+    dict = defaultdict(list)
+    for data in datas:
+        data = list(data)
+        
+        dict[data[1]].append({"type": data[2], "asset": data[3], "equity": data[4], "equity_non": data[5], "liability": data[6],\
+            "current_asset": data[7], "profit": data[8], "profit_non": data[9], "revenue": data[10], "cash": data[11], "ebitda": data[13], \
+                "gross_margin": data[14]})
+
+    return dict
+
+#보조지표 key, value 맞춰주기
+def indi2dict(datas):
+    dict = defaultdict(list)
+    for data in datas:
+        data = list(data)
+
+        dict[data[1]].append({"type": data[2], "eps": data[3], "bps": data[4], "roe": data[5]})
+    
+    return dict
---
+++ testcontroller.py
@@ -0,0 +1,261 @@
+from collections import defaultdict
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from starlette.responses import JSONResponse
+
+import pymysql, json, process_data
+
+today = datetime.now().strftime("%Y-%m-%d")
+stdday = (datetime.now() - relativedelta(years=5)).strftime("%Y-%m-%d")
+
+conn = pymysql.connect(host="127.0.0.1", port=3306, user="root", password="1234", db="capstone", charset="utf8")
+curs = conn.cursor()
+conn.commit()
+
+def lastday():
+    sql = "select date from stock_marcap ORDER BY date DESC limit 1"
+    curs.execute(sql)
+    data = curs.fetchall()
+
+    return data[0][0]
+
+def match_corp():
+    sql = "select code, name from corp_krx"
+    curs.execute(sql)
+    temp = curs.fetchall()
+
+    data = process_data.codelist(temp)
+
+    js = json.dumps(data, ensure_ascii=False)
+    conn.commit()
+    conn.close()
+
+    return data
+
+def match_krx():
+    sql = "select code, name from stock_krx"
+    curs.execute(sql)
+    temp = curs.fetchall()
+
+    data = process_data.codelist(temp)
+
+    js = json.dumps(data, ensure_ascii=False)
+    conn.commit()
+    conn.close()
+
+    return data
+
+def find_daily_total():
+    sql = "Select * from daily_total where date between %s and %s"
+    curs.execute(sql, (stdday, today))
+    temp = list(curs.fetchall())
+
+    data = process_data.dateisidx(temp)
+    js = json.dumps(data, ensure_ascii=False)
+
+    conn.commit()
+    conn.close()
+
+    return data
+
+def daily_rank():
+    last_day = lastday()
+
+    #시총 TOP10
+    sql = "select code, name, close, marcap from stock_marcap where date = %s ORDER BY marcap DESC limit 10" 
+    curs.execute(sql, last_day)
+    data = curs.fetchall()
+
+    dict1 = process_data.findtop("marcap", data)
+
+    #떡상 TOP10
+    sql = "select code, name, close, changes_ratio from stock_marcap where date = %s ORDER BY changes_ratio DESC limit 10" 
+    curs.execute(sql, last_day)
+    data = curs.fetchall()
+
+    dict2 = process_data.findtop("change_incr", data)
+
+    #떡락 TOP 10
+    sql = "select code, name, close, changes_ratio from stock_marcap where date = %s ORDER BY changes_ratio ASC limit 10" 
+    curs.execute(sql, last_day)
+    data = curs.fetchall()
+
+    dict3 = process_data.findtop("change_redu", data)
+
+    #거래량 TOP 10
+    sql = "select code, name, close, volume from stock_marcap where date = %s ORDER BY volume DESC limit 10" 
+    curs.execute(sql, last_day)
+    data = curs.fetchall()
+
+    dict4 = process_data.findtop("volume", data)
+    
+    res = dict(dict1, **dict2, **dict3, **dict4)
+
+    conn.commit()
+    conn.close()
+
+    return res
+
+def find_recommand():
+    dict = defaultdict(list)
+    last_day = lastday()
+
+    sql = "select code from corp_krx ORDER BY RAND() LIMIT 12"
+    curs.execute(sql)
+    datas = curs.fetchall()
+
+    for data in datas:
+        sql = "select code, name, close, changes_ratio from stock_marcap where code = %s and date = %s"
+        curs.execute(sql, (data[0], last_day))
+        temp = curs.fetchall()
+
+        dic = process_data.matchrecom(temp)
+        dict.update(dic)
+    
+    return dict
+
+        
+    
+
+def stock_info(name):
+    sql = "select * from stock_marcap where name = %s ORDER BY date DESC limit 1"
+    curs.execute(sql, name)
+    data = curs.fetchall()
+    
+    if len(data) == 0:
+        return "잘못된 기업명입니다~"
+
+    data = data[0]
+    
+    conn.commit()
+    conn.close()
+
+    return data
+
+def graph2weeks(name):
+    last_day = lastday()
+
+    sql = "select date, close from stock_marcap where name = %s and date >= DATE_ADD(%s, INTERVAL -14 DAY) ORDER BY date ASC limit 14"
+    curs.execute(sql, (name, last_day))
+    data = curs.fetchall()
+    
+    if len(data) == 0:
+        return "잘못된 기업명입니다~"
+    
+    res = process_data.data2grahp(data)
+
+    conn.commit()
+    conn.close()
+
+    return res
+
+def graph5year(name):
+    last_day = lastday()
+
+    sql = "select date, close from stock_marcap where name = %s and date >= DATE_ADD(%s, INTERVAL -5 YEAR)"
+    curs.execute(sql, (name, last_day))
+    data = curs.fetchall()
+
+    if len(data) == 0:
+        return "잘못된 기업명입니다~"
+
+    res = process_data.data2grahp(data)
+
+    conn.commit()
+    conn.close()
+
+    return res
+
+def graph_detail(name, start, end):
+    sql = "select date, close from stock_marcap where name = %s and date between %s and %s"
+    curs.execute(sql, (name, start, end))
+    data = curs.fetchall()
+    if len(data) == 0:
+        return "잘못된 기업명 or 해당 날짜 데이터가 없어요~"
+
+    res = process_data.data2grahp(data)
+
+    conn.commit()
+    conn.close()
+
+    return res
+
+def type2graph(type, name):
+    sql = "select code from corp_krx where name = %s"
+    curs.execute(sql, name)
+    temp = curs.fetchall()
+    
+    if len(temp) == 0:
+        return "선물 혹은 잘못된 기업명이에요~"
+
+    code = temp[0][0]
+    
+    if type == "ebitda":
+        sql = "select date, ebitda from stock_statements where code = %s"
+    elif type == "revenue":
+        sql = "select date, revenue from stock_statements where code = %s"
+
+    curs.execute(sql, code)
+    data = curs.fetchall()
+
+    if len(data) == 0:
+        return "재무제표가 없어요~"
+    
+    res = process_data.data2grahp(data)
+
+    conn.commit()
+    conn.close()
+
+    return res
+
+def find_statement(name):
+    sql = "select code from corp_krx where name = %s"
+    curs.execute(sql, name)
+    temp = curs.fetchall()
+    
+    if len(temp) == 0:
+        return "선물 혹은 잘못된 기업명이에요~"
+
+    code = temp[0][0]
+
+    sql = "select * from stock_statements where code = %s ORDER BY date DESC limit 4"
+    curs.execute(sql, code)
+    
+    data = curs.fetchall()
+    res = process_data.state2dict(data)
+    
+    conn.commit()
+    conn.close()
+    
+    return res
+   
+def find_indicator(name):
+    last_day = lastday()
+
+    sql = "select code from corp_krx where name = %s"
+    curs.execute(sql, name)
+    temp = curs.fetchall()
+    
+    if len(temp) == 0:
+        return "선물 혹은 잘못된 기업명이에요~"
+
+    code = temp[0][0]
+
+    sql = "select * from stock_indicator where code = %s ORDER BY date DESC limit 4"
+    curs.execute(sql, code)
+    
+    data = curs.fetchall()
+    res = process_data.indi2dict(data)
+    
+    sql = "select per, pbr from stock_marcap where name = %s and date = %s"
+    curs.execute(sql, (name, last_day))
+    temp = curs.fetchall()
+    
+    dic = {"per": temp[0][0], "pbr":temp[0][1]}
+
+    res.update(dic)
+
+    conn.commit()
+    conn.close()
+    
+    return res
