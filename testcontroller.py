13e9404187b903be5e95f1fc4835e62660363834
codma <ruddud1123@naver.com>
2022-05-18 17:30:46 +0900

    구버전 init

--- testcontroller.py
+++ testcontroller.py
@@ -1,3 +1,5 @@
+# uvicorn testserver:app --reload
+
 from collections import defaultdict
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
@@ -64,28 +66,28 @@ def find_daily_total():
 
 def daily_rank():
     #시총 TOP10
-    sql = "select code, name, close, marcap from stock_marcap where date = %s ORDER BY marcap DESC limit 10" 
+    sql = "select code, name, close, marcap from stock_marcap where date = %s ORDER BY marcap DESC limit 50" 
     curs.execute(sql, last_day)
     data = curs.fetchall()
 
     dict1 = process_data.findtop("marcap", data)
 
     #떡상 TOP10
-    sql = "select code, name, close, changes_ratio from stock_marcap where date = %s ORDER BY changes_ratio DESC limit 10" 
+    sql = "select code, name, close, changes_ratio from stock_marcap where date = %s ORDER BY changes_ratio DESC limit 50" 
     curs.execute(sql, last_day)
     data = curs.fetchall()
 
     dict2 = process_data.findtop("change_incr", data)
 
     #떡락 TOP 10
-    sql = "select code, name, close, changes_ratio from stock_marcap where date = %s ORDER BY changes_ratio ASC limit 10" 
+    sql = "select code, name, close, changes_ratio from stock_marcap where date = %s ORDER BY changes_ratio ASC limit 50" 
     curs.execute(sql, last_day)
     data = curs.fetchall()
 
     dict3 = process_data.findtop("change_redu", data)
 
     #거래량 TOP 10
-    sql = "select code, name, close, volume from stock_marcap where date = %s ORDER BY volume DESC limit 10" 
+    sql = "select code, name, close, volume from stock_marcap where date = %s ORDER BY volume DESC limit 50" 
     curs.execute(sql, last_day)
     data = curs.fetchall()
 
